6a1cc69a560df8734216d8642c4798bb9737f273
ddudalves <169068338+ddudalves@users.noreply.github.com>
2024-09-03 01:03:36 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.utilScanner;
+
+public class Main {
+  public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
+    System.out.println("Insira sua senha:");
+    int senha = 2022;
+    int inserida = sc.nextInt();
+
+    while ( inserida  != senha){
+      System.out.println("SE")
+        inserida 
+  }
+
+
+}
